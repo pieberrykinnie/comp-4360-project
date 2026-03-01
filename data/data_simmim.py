59e9cea2824f69c772f026ffba26ce121c266b36
Aamir Sangey <asangey11@gmail.com>
2026-02-28 19:56:36 -0600

    created mask Generator and did input validation

--- data/data_simmim.py
+++ data/data_simmim.py
@@ -1,4 +1,9 @@
 import os
 import csv 
+import numpy as np
 from PIL import Image # make sure you have Pillow installed: pip install Pillow
-from torch.utils.data import Dataset
+
+import torch
+import torch.distributed as dist
+from torch.utils.data import Dataset, DataLoader, DistributedSampler
+from torchvision import transforms
