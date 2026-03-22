4b026e3528fdb61cb12a602a4e5d6184c87ffe86
Aamir Sangey <asangey11@gmail.com>
2026-03-22 17:56:36 -0500

    created the reduce_tensor func

--- src/utils.py
+++ src/utils.py
@@ -12,3 +12,14 @@ def is_dist_initialized():
         return False
 
     return True
+
+def reduce_tebsore(tensor):
+    if not is_dist_initialized():
+        return tensor
+
+    reduced_tensor = tensor.clone()
+    # here i am adding together the tensor vals
+    dist.all_reduce(reduced_tensor, op=dist.ReduceOp.SUM)
+    #then do the avg
+    reduced_tensor /= dist.get_world_size()
+    return reduced_tensor
