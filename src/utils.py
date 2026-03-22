ca9044b7112586cf040b847a02c66f792aee53cf
Aamir Sangey <asangey11@gmail.com>
2026-03-22 18:00:13 -0500

    created get_grad_norm

--- src/utils.py
+++ src/utils.py
@@ -23,3 +23,16 @@ def reduce_tebsore(tensor):
     #then do the avg
     reduced_tensor /= dist.get_world_size()
     return reduced_tensor
+
+def get_grad_norm(parameters):
+    total_norm = 0.0
+    #we looping through all the parameters
+    for p in parameters:
+        if p.grad is not None:
+            # we r computing the L2 norm of the gradients
+            param_norm = p.grad.data.norm(2).item()
+            #squeare them
+            total_norm += param_norm ** 2
+    #then we take the square root
+    total_norm = total_norm ** 0.5
+    return total_norm
