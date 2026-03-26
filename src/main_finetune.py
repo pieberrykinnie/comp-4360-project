9b9103eed322bdb2663900ce981b0207a0a9eae2
Manmilan <manmilan@myumanitoba.ca>
2026-03-25 21:47:37 -0500

    fix typo

--- src/main_finetune.py
+++ src/main_finetune.py
@@ -259,7 +259,7 @@ def train_one_epoch(conig, model, criterion, data_loader, optimizer, epoch, mixu
         start = end
         
         if idx % config.PRINT_FREQ == 0:
-            lr = optimzer.param_groups[-1]['lr']
+            lr = optimizer.param_groups[-1]['lr']
             memory_used = torch.cuda.max_memory_allocated() / (1024.0 * 1024.0)
             etas = batch_time.avg * (num_steps - idx)
             logger.info(
