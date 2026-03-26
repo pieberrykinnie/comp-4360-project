0f4a9ba51c2497e6bcc8973219396e3e1b8710f3
Manmilan <manmilan@myumanitoba.ca>
2026-03-25 22:21:48 -0500

    add throughput() to measure inference speed

--- src/main_finetune.py
+++ src/main_finetune.py
@@ -364,7 +364,25 @@ def validate(config, data_loader, model):
     return mean_auc, loss_meter.avg, per_class_auc
 
 
+# measures inference speed
+@torch.no_grad()
+def throughput(data_loader, model, logger):
+    model.eval()
 
+    for idx, (images, targets) in enumerate(data_loader):
+        images = images.cuda(non_blocking=True)
+        batch_size = images.shape[0]
+        for i in range(50):     # warmup
+            model(images)
+        torch.cuda.synchronize()    # waits for GPU to finish work
+        logger.info(f"throughput averaged with 30 times")
+        tic1 = time.time()
+        for i in range(30):     # time this model run (whole 30 times)
+            model(images)
+        torch.cuda.synchronize()
+        tic2 = time.time()
+        logger.info(f"batch_size {batch_size} throughput {30 * batch_size / (tic2 - tic1)}")
+        return
 
 
 if __name__ == '__main__':
