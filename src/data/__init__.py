8b3731f15fed76873da4e66673bfe4abf091e310
Aamir Sangey <asangey11@gmail.com>
2026-03-28 02:18:34 -0500

    added small fix

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,2 +1,8 @@
 from .data_simmim import build_loader_simmim
-from .data_finetune import build_loader_finetune
+
+def build_loader(config, logger, is_pretrain):
+    if is_pretrain:
+        return build_loader_simmim(config, logger)
+    else:
+        from .data_finetune import build_loader_finetune
+        return build_loader_finetune(config, logger)
