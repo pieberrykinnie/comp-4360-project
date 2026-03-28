4fc3196156cb43c70b6632e68001f88e95beb9a6
pieberrykinnie <dmtruong1980@gmail.com>
2026-03-28 14:07:08 -0500

    fix: Fix import shenanigans

--- src/lr_scheduler.py
+++ src/lr_scheduler.py
@@ -2,12 +2,10 @@ from bisect import bisect_right
 from timm.scheduler.cosine_lr import CosineLRScheduler
 from timm.scheduler.step_lr import StepLRScheduler
 from timm.scheduler.scheduler import Scheduler
-
-from src.config import Config
 from torch.optim import Optimizer
 
 
-def build_scheduler(config: Config, optimizer: Optimizer, n_iter_per_epoch: int) -> Scheduler:
+def build_scheduler(config, optimizer: Optimizer, n_iter_per_epoch: int) -> Scheduler:
     num_steps: int = int(config.TRAIN.EPOCHS * n_iter_per_epoch)
     warmup_steps: int = int(config.TRAIN.WARMUP_EPOCHS * n_iter_per_epoch)
     decay_steps: int = int(
